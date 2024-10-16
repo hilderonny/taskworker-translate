8f658ce78777b223c72e16591530815a8c80ae67
hilderonny <hilderonny2014@gmail.com>
2024-10-16 11:05:25 +0200

    Fixed GPU address in shell script

--- translate.sh
+++ translate.sh
@@ -3,4 +3,4 @@
 export LD_LIBRARY_PATH=`./python-venv/bin/python -c 'import os; import nvidia.cublas.lib; import nvidia.cudnn.lib; print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))'`
 export CUDA_VISIBLE_DEVICES=0
 
-./python-venv/bin/python translate.py --taskbridgeurl http://127.0.0.1:42000/ --worker SENECA-GPU0 --device cuda:0
+./python-venv/bin/python translate.py --taskbridgeurl http://127.0.0.1:42000/ --worker SENECA-GPU0 --device cuda
